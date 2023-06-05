a0dbd8229be6bff273a340570906e908df4d544a
Limeshrk <67397781+Limeshrk@users.noreply.github.com>
2023-06-05 11:03:48 +0200

    3_google_sheet.py minor fix

--- 3_google_sheet.py
+++ 3_google_sheet.py
@@ -23,17 +23,20 @@ def get_currency_rate(from_currency, to_currency):
     except Exception as e:
         print(f"hiba az átváltáskor: {str(e)}")
 
-# Munkafüzet sorainak hossza
-num_rows = len(wks.get_all_values())
+def sheet_update():
+    # Munkafüzet sorainak hossza
+    num_rows = len(wks.get_all_values())
 
-# Végig megy minden soron
-for row in range(2, num_rows + 1): # Az első a fejléc
-    moneyfrom = wks.cell(row, 1).value
-    moneyto = wks.cell(row, 2).value
-    rate = get_currency_rate(moneyfrom, moneyto)
+    # Végig megy minden soron
+    for row in range(2, num_rows + 1): # Az első a fejléc
+        moneyfrom = wks.cell(row, 1).value
+        moneyto = wks.cell(row, 2).value
+        rate = get_currency_rate(moneyfrom, moneyto)
 
-    # Frissiti a dokumentumot és kiírja az aktuális értékeket
-    wks.update_cell(row, 3, rate)
-    print(f"Árfolyam frissítve: {moneyfrom} -> {moneyto} : {rate}")
+        # Frissiti a dokumentumot és kiírja az aktuális értékeket
+        wks.update_cell(row, 3, rate)
+        print(f"Árfolyam frissítve: {moneyfrom} -> {moneyto} : {rate}")
 
-print("Az árfolyamok frissítése sikeresen megtörtént.")
+    print("Az árfolyamok frissítése sikeresen megtörtént.")
+    
+sheet_update()
